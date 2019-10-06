949b3d3e118206ac0b5ed07269536ecbede0a12e
coolworld2015 <wintermute2015@ukr.net>
2019-10-06 10:58:21 +0300

    [chat] added chat

--- app/src/ios/app/app.js
+++ app/src/ios/app/app.js
@@ -8,51 +8,52 @@ import AppContainer from './appContainer';
 console.disableYellowBox = true;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      isLoggedIn: true,
-    };
+        this.state = {
+            isLoggedIn: false,
+        };
 
-    window.appConfig = {
-      access_token: '',
-      url: 'http://jwt-base.herokuapp.com/',
-      onLogOut: this.onLogOut.bind(this),
-      phones: {
-        items: [],
-        item: {},
-      },
-      users: {
-        items: [],
-        item: {},
-      },
-      audit: {
-        items: [],
-        item: {},
-      },
-    };
-  }
-
-  onLogin() {
-    this.setState({isLoggedIn: true});
-  }
-
-  onLogOut() {
-    this.setState({isLoggedIn: false});
-  }
-
-  render() {
-    if (this.state.isLoggedIn) {
-      return (
-        <AppContainer/>
-      );
-    } else {
-      return (
-        <Login onLogin={this.onLogin.bind(this)}/>
-      );
+        window.appConfig = {
+            access_token: '',
+            url: 'http://jwt-chat.herokuapp.com/',
+            onLogOut: this.onLogOut.bind(this),
+            socket: {},
+            phones: {
+                items: [],
+                item: {},
+            },
+            users: {
+                items: [],
+                item: {},
+            },
+            audit: {
+                items: [],
+                item: {},
+            },
+        };
     }
-  }
+
+    onLogin() {
+        this.setState({isLoggedIn: true});
+    }
+
+    onLogOut() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        if (this.state.isLoggedIn) {
+            return (
+                <AppContainer/>
+            );
+        } else {
+            return (
+                <Login onLogin={this.onLogin.bind(this)}/>
+            );
+        }
+    }
 }
 
 export default App;
--- app/src/ios/app/appContainer.js
+++ app/src/ios/app/appContainer.js
@@ -5,66 +5,94 @@ import Users from '../users/users';
 import UserDetails from '../users/userDetails';
 import UserAdd from '../users/userAdd';
 
+import Chat from '../chat/chat';
+//import Chat from '../chat/test';
+
 import {Image} from 'react-native';
 
 import {StackViewStyleInterpolator} from 'react-navigation-stack';
 
+const ChatTab = createStackNavigator({
+        Chat,
+        UserDetails,
+        UserAdd,
+    }, {
+        headerMode: 'none',
+        transitionConfig: () => ({
+            screenInterpolator: sceneProps => {
+                return StackViewStyleInterpolator.forHorizontal(sceneProps);
+            },
+        }),
+    },
+);
+
 const UsersTab = createStackNavigator({
-    Users,
-    UserDetails,
-    UserAdd,
-  }, {
-    headerMode: 'none',
-    transitionConfig: () => ({
-      screenInterpolator: sceneProps => {
-        return StackViewStyleInterpolator.forHorizontal(sceneProps);
-      },
-    }),
-  },
+        Users,
+        UserDetails,
+        UserAdd,
+    }, {
+        headerMode: 'none',
+        transitionConfig: () => ({
+            screenInterpolator: sceneProps => {
+                return StackViewStyleInterpolator.forHorizontal(sceneProps);
+            },
+        }),
+    },
 );
 
 class Quit extends React.Component {
-  render() {
-    //window.appConfig.onLogOut();
-    return null;
-  }
+    render() {
+        window.appConfig.onLogOut();
+        return null;
+    }
 }
 
 const TabNavigator = createBottomTabNavigator({
-    Dummy: UsersTab,
-    Quit: Quit
-  },
-  {
-    defaultNavigationOptions: ({navigation}) => ({
-      tabBarIcon: ({focused, horizontal, tintColor}) => {
-        const {routeName} = navigation.state;
-        let iconName;
+        Chat: ChatTab,
+        Dummy: UsersTab,
+        Quit: Quit
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            tabBarIcon: ({focused, horizontal, tintColor}) => {
+                const {routeName} = navigation.state;
+                let iconName;
 
-        if (routeName === 'Dummy') {
-          iconName = <Image
-            source={require('../../../img/users.png')}
-            style={{
-              height: 20,
-              width: 20,
-              margin: 0,
-            }}
-          />;
-        }
-        if (routeName === 'Quit') {
-          iconName = <Image
-            source={require('../../../img/log-out.png')}
-            style={{
-              height: 20,
-              width: 20,
-              margin: 0,
-            }}
-          />;
-        }
+                if (routeName === 'Chat') {
+                    iconName = <Image
+                        source={require('../../../img/users.png')}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            margin: 0,
+                        }}
+                    />;
+                }
+                if (routeName === 'Dummy') {
+                    iconName = <Image
+                        source={require('../../../img/clock.png')}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            margin: 0,
+                        }}
+                    />;
+                }
+                if (routeName === 'Quit') {
+                    iconName = <Image
+                        source={require('../../../img/log-out.png')}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            margin: 0,
+                        }}
+                    />;
+                }
 
-        return iconName;
-      },
-    }),
-  },
+                return iconName;
+            },
+        }),
+    },
 );
 
 export default createAppContainer(TabNavigator);
